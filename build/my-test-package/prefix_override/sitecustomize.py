67c9282c1cecf5ec632f4fc18d3632a1e4458674
Nazar Dobrovolskyi <yfpfh233@gmail.com>
2024-09-19 21:54:45 +0300

    Remove build log and install folders

--- build/my-test-package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/nazar/dev/ROS2/install/my-test-package'
--- build/toxic_relationship/ament_cmake_python/toxic_relationship/toxic_relationship/action/__init__.py
+++
@@ -1,1 +0,0 @@
-from toxic_relationship.action._toxic_relationship import ToxicRelationship  # noqa: F401

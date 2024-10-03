0bbab0d99772522898b51cb4b9b8840e69f30340
Nazar Dobrovolskyi <yfpfh233@gmail.com>
2024-10-03 15:06:07 +0300

    Add ability to choose and take params in launch from specific YAML file

--- src/py_pubsub/setup.py
+++ src/py_pubsub/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'py_pubsub'
 
@@ -10,7 +11,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='nazar',
